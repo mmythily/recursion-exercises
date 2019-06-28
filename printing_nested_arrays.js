ad225d9f320b6d429e44b6bf92b68b1f2f430d8d
mmythily <mythily.mudunuru@gmail.com>
2019-06-28 02:38:38 -0400

    nested array recursion

---
+++ printing_nested_arrays.js
@@ -0,0 +1,15 @@
+function printItems(array) {
+    array.forEach((item) => {
+        if (Array.isArray(item)) {
+          // Print out all it's items individually
+            printItems(item);
+        } else {
+            console.log(item);
+        }
+    });
+}
+
+const array = ["😎", ["💩", "🤗"], "😼", "😂"];
+const array1 = ["😎", [["💩", ["🤗"]], [[["😼"]], "😂"]]];
+printItems(array);
+printItems(array1);
